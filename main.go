aacb731ec117a71948a94a434d87e0f1ab543542
Ziyi Yan <ziyi.yan@foxmail.com>
2019-07-28 00:51:22 +0800

    implement cronjob webhook
    reference: https://book.kubebuilder.io/cronjob-tutorial/webhook-implementation.html

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ import (
 	"flag"
 	"os"
 
+	kbatchv1 "k8s.io/api/batch/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -37,6 +38,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = kbatchv1.AddToScheme(scheme) // we've added this ourselves
 	_ = batchv1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
@@ -66,6 +68,18 @@ func main() {
 		Log:    ctrl.Log.WithName("controllers").WithName("CronJob"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "CronJob")
+		os.Exit(1)
+	}
+	if err = (&controllers.CronJobReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Captain"),
+		Scheme: mgr.GetScheme(), // we've added this ourselves
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Captain")
+		os.Exit(1)
+	}
+	if err = (&batchv1.CronJob{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Captain")
 		os.Exit(1)
 	}
 	// +kubebuilder:scaffold:builder
